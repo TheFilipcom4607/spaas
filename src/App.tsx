c99dce9055b9166c4e90fdce6d827bc4ac0a97ce
TheFilip <121570139+TheFilipcom4607@users.noreply.github.com>
2026-04-06 21:42:05 +0200

    Rename import from Joke to Payment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Joke from './pages/Payment.tsx';
+import Payment from './pages/Payment';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import DotGrid from './components/DotGrid';
